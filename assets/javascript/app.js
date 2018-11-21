fd2ee12678e6659d9c4519b6082669a7cea0ac29
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-11-21 15:24:19 -0800

    add array of dance styles to generate buttons then gifs from

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,5 @@
 // GIPHY API key: hKygJ8O2rMKyZ8WKPW0bLfaRtHkXG5Vk
+
+// Array to hold the default/displayed styles of dance
+var defaultStyles = ["swing dancing", "tango", "alcha gulu", "locking", "line dancing", "krumping", "pogo dancing", "baroque dancing", "ballet dancing", "belly dancing", "samba", "tap dancing", "square dancing", "irish step dancing", "melbourne shuffle"];
+
